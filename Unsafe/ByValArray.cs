66923c4ceaceaf599230ae116fc1ec1094d75807
IllidanS4 <illidans4@gmail.com>
2017-08-20 14:34:34 +0200

    Identity for value types and example

---
+++ Unsafe/ByValArray.cs
@@ -0,0 +1,202 @@
+﻿/* Date: 18.8.2017, Time: 13:57 */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IllidanS4.SharpUtils.Unsafe
+{
+	/// <summary>
+	/// A dynamically sized array with value type semantics.
+	/// </summary>
+	public struct ByValArray<T> : IList<T>, IList
+	{
+		T[] data;
+		
+		public ByValArray(int length)
+		{
+			data = new T[length];
+		}
+		
+		private void Update()
+		{
+			if(data == null) data = new T[0];
+			data = ReferenceStorage.FindInstance(ref this, data);
+		}
+		
+		public int Length{
+			get{
+				Update();
+				return data.Length;
+			}
+		}
+		
+		int ICollection<T>.Count{
+			get{
+				return Length;
+			}
+		}
+		
+		int ICollection.Count{
+			get{
+				return Length;
+			}
+		}
+		
+		public T this[int index]
+		{
+			get{
+				Update();
+				return data[index];
+			}
+			set{
+				Update();
+				data[index] = value;
+			}
+		}
+		
+		public bool IsReadOnly{
+			get{
+				return false;
+			}
+		}
+		
+		int IList<T>.IndexOf(T item)
+		{
+			Update();
+			return ((IList<T>)data).IndexOf(item);
+		}
+		
+		void IList<T>.Insert(int index, T item)
+		{
+			Update();
+			((IList<T>)data).Insert(index, item);
+		}
+		
+		void IList<T>.RemoveAt(int index)
+		{
+			Update();
+			((IList<T>)data).RemoveAt(index);
+		}
+		
+		void IList.RemoveAt(int index)
+		{
+			Update();
+			((IList)data).RemoveAt(index);
+		}
+		
+		void ICollection<T>.Add(T item)
+		{
+			Update();
+			((ICollection<T>)data).Add(item);
+		}
+		
+		void ICollection<T>.Clear()
+		{
+			Update();
+			((ICollection<T>)data).Clear();
+		}
+		
+		void IList.Clear()
+		{
+			Update();
+			((IList)data).Clear();
+		}
+		
+		bool ICollection<T>.Contains(T item)
+		{
+			Update();
+			return ((IList<T>)data).Contains(item);
+		}
+		
+		void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+		{
+			Update();
+			((IList<T>)data).CopyTo(array, arrayIndex);
+		}
+		
+		bool ICollection<T>.Remove(T item)
+		{
+			Update();
+			return ((ICollection<T>)data).Remove(item);
+		}
+		
+		public IEnumerator<T> GetEnumerator()
+		{
+			Update();
+			return ((IEnumerable<T>)data).GetEnumerator();
+		}
+		
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			Update();
+			return ((IEnumerable)data).GetEnumerator();
+		}
+		
+		object IList.this[int index]
+		{
+			get{
+				Update();
+				return ((IList)data)[index];
+			}
+			set{
+				Update();
+				((IList)data)[index] = value;
+			}
+		}
+		
+		bool IList.IsFixedSize{
+			get{
+				Update();
+				return ((IList)data).IsFixedSize;
+			}
+		}
+		
+		object ICollection.SyncRoot{
+			get{
+				return null;
+			}
+		}
+		
+		bool ICollection.IsSynchronized{
+			get{
+				return false;
+			}
+		}
+		
+		int IList.Add(object value)
+		{
+			Update();
+			return ((IList)data).Add(value);
+		}
+		
+		bool IList.Contains(object value)
+		{
+			Update();
+			return ((IList)data).Contains(value);
+		}
+		
+		int IList.IndexOf(object value)
+		{
+			Update();
+			return ((IList)data).IndexOf(value);
+		}
+		
+		void IList.Insert(int index, object value)
+		{
+			Update();
+			((IList)data).Insert(index, value);
+		}
+		
+		void IList.Remove(object value)
+		{
+			Update();
+			((IList)data).Remove(value);
+		}
+		
+		void ICollection.CopyTo(Array array, int index)
+		{
+			Update();
+			((ICollection)data).CopyTo(array, index);
+		}
+	}
+}
---
+++ Unsafe/ReferenceStorage.cs
@@ -0,0 +1,61 @@
+﻿/* Date: 9.8.2017, Time: 1:30 */
+using System;
+using System.Collections.Generic;
+
+namespace IllidanS4.SharpUtils.Unsafe
+{
+	/// <summary>
+	/// This class can be used to enforce value-type semantics onto
+	/// a reference-containing structure.
+	/// </summary>
+	public static class ReferenceStorage
+	{
+		/// <summary>
+		/// Call this method from an "Update" method inside the struct, passing
+		/// "ref this" as the first argument, and its field as the second.
+		/// Store the returned object in the field.
+		/// </summary>
+		/// <param name="val">The reference to index with.</param>
+		/// <param name="orig">The original value of the field.</param>
+		/// <returns>The new value of the field.</returns>
+		/// <typeparam name="T">
+		/// The structure should contain a single private field, having the type <typeparamref name="TData"/>.
+		/// </typeparam>
+		/// <typeparam name="TData">
+		/// The underlying data of the structure.
+		/// </typeparam>
+		public static TData FindInstance<T, TData>(ref T val, TData orig) where T : struct where TData : class, ICloneable
+		{
+			return Storage<T, TData>.FindInstance(ref val, orig);
+		}
+		
+		static class Storage<T, TData> where T : struct where TData : class, ICloneable
+		{
+			static readonly Dictionary<IntPtr, WeakReference<TData>> Cache = new Dictionary<IntPtr, WeakReference<TData>>();
+			
+			public static TData FindInstance(ref T val, TData orig)
+			{
+				return UnsafeTools.GetPointer(
+					out val,
+					ptr => {
+						WeakReference<TData> wref;
+						TData inst;
+						if(Cache.TryGetValue(ptr, out wref) && wref.TryGetTarget(out inst))
+						{
+							if(inst != orig)
+							{
+								inst = (TData)orig.Clone();
+								Cache[ptr] = new WeakReference<TData>(inst);
+							}
+							return inst;
+						}else{
+							inst = (TData)orig.Clone();
+							Cache[ptr] = new WeakReference<TData>(inst);
+							return inst;
+						}
+					}
+				);
+			}
+		}
+	}
+}
