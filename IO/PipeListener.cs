c64404c7859aba5164201f84bedc952f5fbb082b
IllidanS4 <illidans4@gmail.com>
2017-09-07 13:50:54 +0200

    Pipe support

---
+++ IO/PipeListener.cs
@@ -0,0 +1,41 @@
+﻿/* Date: 7.9.2017, Time: 13:01 */
+using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IllidanS4.SharpUtils.IO
+{
+	public class PipeListener
+	{
+		public string Name{get; private set;}
+		public PipeDirection Direction{get; private set;}
+		public PipeTransmissionMode TransmissionMode{get; private set;}
+		
+		public PipeListener(string pipeName, PipeDirection direction) : this(pipeName, direction, PipeTransmissionMode.Byte)
+		{
+			
+		}
+		
+		public PipeListener(string pipeName, PipeDirection direction, PipeTransmissionMode transmissionMode)
+		{
+			Name = pipeName;
+			Direction = direction;
+			TransmissionMode = transmissionMode;
+		}
+		
+		public NamedPipeServerStream AcceptClient()
+		{
+			var stream = new NamedPipeServerStream(Name, Direction, NamedPipeServerStream.MaxAllowedServerInstances, TransmissionMode);
+			stream.WaitForConnection();
+			return stream;
+		}
+		
+		public async Task<NamedPipeServerStream> AcceptClientAsync(CancellationToken cancellationToken)
+		{
+			var stream = new NamedPipeServerStream(Name, Direction, NamedPipeServerStream.MaxAllowedServerInstances, TransmissionMode, PipeOptions.Asynchronous);
+			await stream.WaitForConnectionAsync(cancellationToken);
+			return stream;
+		}
+	}
+}
---
+++ IO/PipeServer.cs
@@ -0,0 +1,75 @@
+﻿/* Date: 7.9.2017, Time: 13:23 */
+using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IllidanS4.SharpUtils.IO
+{
+	public class PipeServer
+	{
+		public string PipeName{get; set;}
+		public PipeDirection PipeDirection{get; set;}
+		public PipeTransmissionMode PipeTransmissionMode{get; set;}
+		
+		public PipeListener Listener{get; private set;}
+		
+		public event EventHandler<NamedPipeEventArgs> PipeOpened;
+		
+		private CancellationTokenSource tokenSource;
+		
+		public PipeServer()
+		{
+			PipeDirection = PipeDirection.Out;
+			PipeTransmissionMode = PipeTransmissionMode.Byte;
+		}
+		
+		public void Start()
+		{
+			if(Listener != null) throw new InvalidOperationException("The server is already running.");
+			Listener = new PipeListener(PipeName, PipeDirection, PipeTransmissionMode);
+			tokenSource = new CancellationTokenSource();
+			
+			CreateServerTask(tokenSource.Token);
+		}
+		
+		public void Stop()
+		{
+			if(Listener == null) throw new InvalidOperationException("The server is not running.");
+			
+			tokenSource.Cancel();
+		}
+		
+		private Task CreateServerTask(CancellationToken token)
+		{
+			return Task.Run(
+				(Func<Task>)async delegate{
+					while(!token.IsCancellationRequested)
+					{
+						var stream = await Listener.AcceptClientAsync(token);
+						OnPipeOpened(stream);
+					}
+				}, token
+			);
+		}
+		
+		private void OnPipeOpened(NamedPipeServerStream stream)
+		{
+			if(PipeOpened != null)
+			{
+				PipeOpened(this, new NamedPipeEventArgs(stream));
+			}
+		}
+	}
+	
+	[Serializable]
+	public class NamedPipeEventArgs : EventArgs
+	{
+		public NamedPipeServerStream Stream{get; private set;}
+		
+		public NamedPipeEventArgs(NamedPipeServerStream pipeStream)
+		{
+			Stream = pipeStream;
+		}
+	}
+}
