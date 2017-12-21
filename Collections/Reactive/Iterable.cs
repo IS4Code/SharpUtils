249fe8ceb82fddf96519bef2fdbd40f0a185fe47
IllidanS4 <illidans4@gmail.com>
2017-12-21 10:38:46 +0100

    More iterable LINQ and checks

--- Collections/Reactive/Iterable.cs
+++ Collections/Reactive/Iterable.cs
@@ -5,20 +5,20 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 {
 	public static class Iterable
 	{
-		private abstract class LinkIterable<TResult> : IIterable<TResult>
+		private interface ILinkIterable<TResult> : IIterable<TResult>
 		{
-			public abstract void Iterate(IIterator<TResult> iterator);
+			ILinkIterable<TNewResult> Link<TNewResult>(IIteratorLink<TResult, TNewResult> link);
 			
-			public abstract LinkIterable<TNewResult> Link<TNewResult>(IIteratorLink<TResult, TNewResult> link);
+			ILinkIterable<TNewResult> Select<TNewResult>(Func<TResult, TNewResult> selector);
 			
-			public abstract LinkIterable<TNewResult> Select<TNewResult>(Func<TResult, TNewResult> selector);
+			ILinkIterable<TResult> Where(Func<TResult, bool> predicate);
 			
-			public abstract LinkIterable<TResult> Where(Func<TResult, bool> predicate);
+			ILinkIterable<TNewResult> Aggregate<TAccumulate, TNewResult>(TAccumulate seed, Func<TAccumulate, TResult, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector);
 			
-			public abstract LinkIterable<TNewResult> Aggregate<TAccumulate, TNewResult>(TAccumulate seed, Func<TAccumulate, TResult, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector);
+			ILinkIterable<TResult> Sum();
 		}
 		
-		private class LinkIterable<TSource, TResult> : LinkIterable<TResult>
+		private class LinkIterable<TSource, TResult> : ILinkIterable<TResult>
 		{
 			readonly IIterable<TSource> iterable;
 			readonly IIteratorLink<TSource, TResult> link;
@@ -29,7 +29,7 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 				this.link = link;
 			}
 			
-			public override void Iterate(IIterator<TResult> iterator)
+			public void Iterate(IIterator<TResult> iterator)
 			{
 				using(var handle = link.Subscribe(iterator))
 				{
@@ -37,30 +37,76 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 				}
 			}
 			
-			public override LinkIterable<TNewResult> Link<TNewResult>(IIteratorLink<TResult, TNewResult> link)
+			public ILinkIterable<TNewResult> Link<TNewResult>(IIteratorLink<TResult, TNewResult> link)
 			{
 				return new LinkIterable<TSource, TNewResult>(iterable, Iterator.Link(this.link, link));
 			}
 			
-			public override LinkIterable<TNewResult> Select<TNewResult>(Func<TResult, TNewResult> selector)
+			public ILinkIterable<TNewResult> Select<TNewResult>(Func<TResult, TNewResult> selector)
 			{
 				return new LinkIterable<TSource, TNewResult>(iterable, Iterator.Select(this.link, selector));
 			}
 			
-			public override LinkIterable<TResult> Where(Func<TResult, bool> predicate)
+			public ILinkIterable<TResult> Where(Func<TResult, bool> predicate)
 			{
 				return new LinkIterable<TSource, TResult>(iterable, Iterator.Where(this.link, predicate));
 			}
 			
-			public override LinkIterable<TNewResult> Aggregate<TAccumulate, TNewResult>(TAccumulate seed, Func<TAccumulate, TResult, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector)
+			public ILinkIterable<TNewResult> Aggregate<TAccumulate, TNewResult>(TAccumulate seed, Func<TAccumulate, TResult, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector)
 			{
 				return new LinkIterable<TSource, TNewResult>(iterable, Iterator.Aggregate(this.link, seed, func, resultSelector));
+			}
+			
+			public ILinkIterable<TResult> Sum()
+			{
+				Type type = typeof(TResult);
+				Type underlyingType = Nullable.GetUnderlyingType(type);
+				
+				bool nullable;
+				TypeCode typeCode;
+				if(underlyingType != null)
+				{
+					nullable = true;
+					typeCode = Type.GetTypeCode(underlyingType);
+				}else{
+					nullable = false;
+					typeCode = Type.GetTypeCode(type);
+				}
+				
+				switch(typeCode)
+				{
+					case TypeCode.Int32:
+						return nullable ? (ILinkIterable<TResult>)(object)new LinkIterable<TSource, int?>(iterable, Iterator.Sum((IIteratorLink<TSource, int?>)this.link)) : (ILinkIterable<TResult>)(object)new LinkIterable<TSource, int>(iterable, Iterator.Sum((IIteratorLink<TSource, int>)this.link));
+					case TypeCode.Int64:
+						return nullable ? (ILinkIterable<TResult>)(object)new LinkIterable<TSource, long?>(iterable, Iterator.Sum((IIteratorLink<TSource, long?>)this.link)) : (ILinkIterable<TResult>)(object)new LinkIterable<TSource, long>(iterable, Iterator.Sum((IIteratorLink<TSource, long>)this.link));
+					case TypeCode.Single:
+						return nullable ? (ILinkIterable<TResult>)(object)new LinkIterable<TSource, float?>(iterable, Iterator.Sum((IIteratorLink<TSource, float?>)this.link)) : (ILinkIterable<TResult>)(object)new LinkIterable<TSource, float>(iterable, Iterator.Sum((IIteratorLink<TSource, float>)this.link));
+					case TypeCode.Double:
+						return nullable ? (ILinkIterable<TResult>)(object)new LinkIterable<TSource, double?>(iterable, Iterator.Sum((IIteratorLink<TSource, double?>)this.link)) : (ILinkIterable<TResult>)(object)new LinkIterable<TSource, double>(iterable, Iterator.Sum((IIteratorLink<TSource, double>)this.link));
+					case TypeCode.Decimal:
+						return nullable ? (ILinkIterable<TResult>)(object)new LinkIterable<TSource, decimal?>(iterable, Iterator.Sum((IIteratorLink<TSource, decimal?>)this.link)) : (ILinkIterable<TResult>)(object)new LinkIterable<TSource, decimal>(iterable, Iterator.Sum((IIteratorLink<TSource, decimal>)this.link));
+					default:
+						throw new NotImplementedException();
+				}
+			}
+			
+			public ILinkIterable<TNewResult> Process<TNewResult>(Func<Func<TNewResult, bool>, Action, bool> processingFunc)
+			{
+				return new LinkIterable<TSource, TNewResult>(iterable, Iterator.Process(this.link, processingFunc));
+			}
+			
+			public ILinkIterable<TNewResult> Process<TNewResult>(Action<Func<TNewResult, bool>, Action> processingAction)
+			{
+				return new LinkIterable<TSource, TNewResult>(iterable, Iterator.Process(this.link, processingAction));
 			}
 		}
 		
 		public static IIterable<TResult> Link<TSource, TResult>(this IIterable<TSource> source, IIteratorLink<TSource, TResult> link)
 		{
-			var linkIterable = source as LinkIterable<TSource>;
+			if(source == null) throw new ArgumentNullException("source");
+			if(link == null) throw new ArgumentNullException("link");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
 			if(linkIterable != null)
 			{
 				return linkIterable.Link(link);
@@ -70,7 +116,10 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 		
 		public static IIterable<TResult> Select<TSource, TResult>(this IIterable<TSource> source, Func<TSource, TResult> selector)
 		{
-			var linkIterable = source as LinkIterable<TSource>;
+			if(source == null) throw new ArgumentNullException("source");
+			if(selector == null) throw new ArgumentNullException("selector");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
 			if(linkIterable != null)
 			{
 				return linkIterable.Select(selector);
@@ -80,7 +129,10 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 		
 		public static IIterable<TSource> Where<TSource>(this IIterable<TSource> source, Func<TSource, bool> predicate)
 		{
-			var linkIterable = source as LinkIterable<TSource>;
+			if(source == null) throw new ArgumentNullException("source");
+			if(predicate == null) throw new ArgumentNullException("predicate");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
 			if(linkIterable != null)
 			{
 				return linkIterable.Where(predicate);
@@ -88,14 +140,299 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 			return new LinkIterable<TSource, TSource>(source, Iterator.Where(predicate));
 		}
 		
-		public static IIterable<TNewResult> Aggregate<TSource, TAccumulate, TNewResult>(this IIterable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector)
+		public static IIterable<TNewResult> AggregateMany<TSource, TAccumulate, TNewResult>(this IIterable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector)
 		{
-			var linkIterable = source as LinkIterable<TSource>;
+			if(source == null) throw new ArgumentNullException("source");
+			if(func == null) throw new ArgumentNullException("func");
+			if(resultSelector == null) throw new ArgumentNullException("resultSelector");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
 			if(linkIterable != null)
 			{
 				return linkIterable.Aggregate(seed, func, resultSelector);
 			}
 			return new LinkIterable<TSource, TNewResult>(source, Iterator.Aggregate(seed, func, resultSelector));
+		}
+		
+		public static TNewResult Aggregate<TSource, TAccumulate, TNewResult>(this IIterable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate, TNewResult> resultSelector)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			if(func == null) throw new ArgumentNullException("func");
+			if(resultSelector == null) throw new ArgumentNullException("resultSelector");
+			
+			source.ForEach(
+				o => seed = func(seed, o)
+			);
+			
+			return resultSelector(seed);
+		}
+		
+		public static IIterable<int> SumMany(this IIterable<int> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<int>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<int, int>(source, Iterator.Sum(default(IIteratorLink<int, int>)));
+		}
+		
+		public static IIterable<long> SumMany(this IIterable<long> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<long>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<long, long>(source, Iterator.Sum(default(IIteratorLink<long, long>)));
+		}
+		
+		public static IIterable<float> SumMany(this IIterable<float> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<float>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<float, float>(source, Iterator.Sum(default(IIteratorLink<float, float>)));
+		}
+		
+		public static IIterable<double> SumMany(this IIterable<double> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<double>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<double, double>(source, Iterator.Sum(default(IIteratorLink<double, double>)));
+		}
+		
+		public static IIterable<decimal> SumMany(this IIterable<decimal> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<decimal>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<decimal, decimal>(source, Iterator.Sum(default(IIteratorLink<decimal, decimal>)));
+		}
+		
+		
+		public static IIterable<int?> SumMany(this IIterable<int?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<int?>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<int?, int?>(source, Iterator.Sum(default(IIteratorLink<int?, int?>)));
+		}
+		
+		public static IIterable<long?> SumMany(this IIterable<long?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<long?>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<long?, long?>(source, Iterator.Sum(default(IIteratorLink<long?, long?>)));
+		}
+		
+		public static IIterable<float?> SumMany(this IIterable<float?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<float?>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<float?, float?>(source, Iterator.Sum(default(IIteratorLink<float?, float?>)));
+		}
+		
+		public static IIterable<double?> SumMany(this IIterable<double?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<double?>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<double?, double?>(source, Iterator.Sum(default(IIteratorLink<double?, double?>)));
+		}
+		
+		public static IIterable<decimal?> SumMany(this IIterable<decimal?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			var linkIterable = source as ILinkIterable<decimal?>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Sum();
+			}
+			return new LinkIterable<decimal?, decimal?>(source, Iterator.Sum(default(IIteratorLink<decimal?, decimal?>)));
+		}
+		
+		public static int Sum(this IIterable<int> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			int sum = 0;
+			source.ForEach(
+				o => sum = checked(sum + o)
+			);
+			
+			return sum;
+		}
+		
+		public static long Sum(this IIterable<long> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			long sum = 0;
+			source.ForEach(
+				o => sum = checked(sum + o)
+			);
+			
+			return sum;
+		}
+		
+		public static float Sum(this IIterable<float> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			float sum = 0;
+			source.ForEach(
+				o => sum = sum + o
+			);
+			
+			return sum;
+		}
+		
+		public static double Sum(this IIterable<double> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			double sum = 0;
+			source.ForEach(
+				o => sum = sum + o
+			);
+			
+			return sum;
+		}
+		
+		public static decimal Sum(this IIterable<decimal> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			decimal sum = 0;
+			source.ForEach(
+				o => sum = sum + o
+			);
+			
+			return sum;
+		}
+		
+		
+		public static int? Sum(this IIterable<int?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			int sum = 0;
+			source.ForEach(
+				o => sum = checked(sum + o ?? 0)
+			);
+			
+			return sum;
+		}
+		
+		public static long? Sum(this IIterable<long?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			long sum = 0;
+			source.ForEach(
+				o => sum = checked(sum + o ?? 0)
+			);
+			
+			return sum;
+		}
+		
+		public static float? Sum(this IIterable<float?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			float sum = 0;
+			source.ForEach(
+				o => sum = sum + o ?? 0
+			);
+			
+			return sum;
+		}
+		
+		public static double? Sum(this IIterable<double?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			double sum = 0;
+			source.ForEach(
+				o => sum = sum + o ?? 0
+			);
+			
+			return sum;
+		}
+		
+		public static decimal? Sum(this IIterable<decimal?> source)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			
+			decimal sum = 0;
+			source.ForEach(
+				o => sum = sum + o ?? 0
+			);
+			
+			return sum;
+		}
+		
+		public static IIterable<TResult> Process<TSource, TResult>(this IIterable<TSource> source, Func<Func<TResult, bool>, Action, bool> processingFunc)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			if(processingFunc == null) throw new ArgumentNullException("processingFunc");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Process(processingFunc);
+			}
+			return new LinkIterable<TSource, TResult>(source, Iterator.Process<TSource, TResult>(processingFunc));
+		}
+		
+		public static IIterable<TResult> Process<TSource, TResult>(this IIterable<TSource> source, Action<Func<TResult, bool>, Action> processingAction)
+		{
+			if(source == null) throw new ArgumentNullException("source");
+			if(processingAction == null) throw new ArgumentNullException("processingFunc");
+			
+			var linkIterable = source as ILinkIterable<TSource>;
+			if(linkIterable != null)
+			{
+				return linkIterable.Process(processingAction);
+			}
+			return new LinkIterable<TSource, TResult>(source, Iterator.Process<TSource, TResult>(processingAction));
 		}
 	}
 }
--- Collections/Reactive/Iterator.cs
+++ Collections/Reactive/Iterator.cs
@@ -128,18 +128,30 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 		
 		public static IIteratorLink<TSource, TResult> Aggregate<TSource, TIntermediate, TAccumulate, TResult>(this IIteratorLink<TSource, TIntermediate> source, TAccumulate seed, Func<TAccumulate, TIntermediate, TAccumulate> func, Func<TAccumulate, TResult> resultSelector)
 		{
-			return new AggregateIterator<TSource, TIntermediate, TAccumulate, TResult>(source, seed, func, resultSelector);
+			return new CustomAggregateIterator<TSource, TIntermediate, TAccumulate, TResult>(source, seed, func, resultSelector);
 		}
 		
-		private class AggregateIterator<TSource, TIntermediate, TAccumulate, TResult> : BasicIteratorLink<TSource, TIntermediate, TResult>
+		private abstract class AggregateIterator<TSource, TIntermediate, TAccumulate, TResult> : BasicIteratorLink<TSource, TIntermediate, TResult>
+		{
+			protected TAccumulate accumulate;
+			
+			public AggregateIterator(IIteratorLink<TSource, TIntermediate> source) : base(source)
+			{
+				
+			}
+			
+			protected abstract override bool OnNextInternal(TIntermediate value);
+			
+			protected abstract override void OnCompletedInternal();
+		}
+		
+		private class CustomAggregateIterator<TSource, TIntermediate, TAccumulate, TResult> : AggregateIterator<TSource, TIntermediate, TAccumulate, TResult>
 		{
 			readonly TAccumulate seed;
 			readonly Func<TAccumulate, TIntermediate, TAccumulate> func;
 			readonly Func<TAccumulate, TResult> resultSelector;
 			
-			TAccumulate accumulate;
-			
-			public AggregateIterator(IIteratorLink<TSource, TIntermediate> source, TAccumulate seed, Func<TAccumulate, TIntermediate, TAccumulate> func, Func<TAccumulate, TResult> resultSelector) : base(source)
+			public CustomAggregateIterator(IIteratorLink<TSource, TIntermediate> source, TAccumulate seed, Func<TAccumulate, TIntermediate, TAccumulate> func, Func<TAccumulate, TResult> resultSelector) : base(source)
 			{
 				this.seed = seed;
 				this.func = func;
@@ -160,6 +172,277 @@ namespace IllidanS4.SharpUtils.Collections.Reactive
 				OnCompletedFinal();
 				accumulate = seed;
 			}
+		}
+		
+		public static IIteratorLink<TSource, int> Sum<TSource>(this IIteratorLink<TSource, int> source)
+		{
+			return new Int32SumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, long> Sum<TSource>(this IIteratorLink<TSource, long> source)
+		{
+			return new Int64SumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, float> Sum<TSource>(this IIteratorLink<TSource, float> source)
+		{
+			return new SingleSumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, double> Sum<TSource>(this IIteratorLink<TSource, double> source)
+		{
+			return new DoubleSumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, decimal> Sum<TSource>(this IIteratorLink<TSource, decimal> source)
+		{
+			return new DecimalSumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, int?> Sum<TSource>(this IIteratorLink<TSource, int?> source)
+		{
+			return new NullableInt32SumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, long?> Sum<TSource>(this IIteratorLink<TSource, long?> source)
+		{
+			return new NullableInt64SumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, float?> Sum<TSource>(this IIteratorLink<TSource, float?> source)
+		{
+			return new NullableSingleSumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, double?> Sum<TSource>(this IIteratorLink<TSource, double?> source)
+		{
+			return new NullableDoubleSumIterator<TSource>(source);
+		}
+		
+		public static IIteratorLink<TSource, decimal?> Sum<TSource>(this IIteratorLink<TSource, decimal?> source)
+		{
+			return new NullableDecimalSumIterator<TSource>(source);
+		}
+		
+		private abstract class SumIterator<TSource, TElement> : AggregateIterator<TSource, TElement, TElement, TElement>
+		{
+			public SumIterator(IIteratorLink<TSource, TElement> source) : base(source)
+			{
+				
+			}
+			
+			protected abstract TElement Add(TElement a, TElement b);
+			
+			protected sealed override bool OnNextInternal(TElement value)
+			{
+				accumulate = Add(accumulate, value);
+				return true;
+			}
+			
+			protected sealed override void OnCompletedInternal()
+			{
+				OnNextFinal(accumulate);
+				OnCompletedFinal();
+				accumulate = default(TElement);
+			}
+		}
+		
+		private abstract class NullableSumIterator<TSource, TElement> : AggregateIterator<TSource, TElement?, TElement, TElement?> where TElement : struct
+		{
+			public NullableSumIterator(IIteratorLink<TSource, TElement?> source) : base(source)
+			{
+				
+			}
+			
+			protected abstract TElement Add(TElement a, TElement b);
+			
+			protected sealed override bool OnNextInternal(TElement? value)
+			{
+				if(value != null)
+				{
+					accumulate = Add(accumulate, (TElement)value);
+				}
+				return true;
+			}
+			
+			protected sealed override void OnCompletedInternal()
+			{
+				OnNextFinal(accumulate);
+				OnCompletedFinal();
+				accumulate = default(TElement);
+			}
+		}
+		
+		private class Int32SumIterator<TSource> : SumIterator<TSource, int>
+		{
+			public Int32SumIterator(IIteratorLink<TSource, int> source) : base(source)
+			{
+				
+			}
+			
+			protected override int Add(int a, int b)
+			{
+				return checked(a + b);
+			}
+		}
+		
+		private class Int64SumIterator<TSource> : SumIterator<TSource, long>
+		{
+			public Int64SumIterator(IIteratorLink<TSource, long> source) : base(source)
+			{
+				
+			}
+			
+			protected override long Add(long a, long b)
+			{
+				return checked(a + b);
+			}
+		}
+		
+		private class SingleSumIterator<TSource> : SumIterator<TSource, float>
+		{
+			public SingleSumIterator(IIteratorLink<TSource, float> source) : base(source)
+			{
+				
+			}
+			
+			protected override float Add(float a, float b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class DoubleSumIterator<TSource> : SumIterator<TSource, double>
+		{
+			public DoubleSumIterator(IIteratorLink<TSource, double> source) : base(source)
+			{
+				
+			}
+			
+			protected override double Add(double a, double b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class DecimalSumIterator<TSource> : SumIterator<TSource, decimal>
+		{
+			public DecimalSumIterator(IIteratorLink<TSource, decimal> source) : base(source)
+			{
+				
+			}
+			
+			protected override decimal Add(decimal a, decimal b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class NullableInt32SumIterator<TSource> : NullableSumIterator<TSource, int>
+		{
+			public NullableInt32SumIterator(IIteratorLink<TSource, int?> source) : base(source)
+			{
+				
+			}
+			
+			protected override int Add(int a, int b)
+			{
+				return checked(a + b);
+			}
+		}
+		
+		private class NullableInt64SumIterator<TSource> : NullableSumIterator<TSource, long>
+		{
+			public NullableInt64SumIterator(IIteratorLink<TSource, long?> source) : base(source)
+			{
+				
+			}
+			
+			protected override long Add(long a, long b)
+			{
+				return checked(a + b);
+			}
+		}
+		
+		private class NullableSingleSumIterator<TSource> : NullableSumIterator<TSource, float>
+		{
+			public NullableSingleSumIterator(IIteratorLink<TSource, float?> source) : base(source)
+			{
+				
+			}
+			
+			protected override float Add(float a, float b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class NullableDoubleSumIterator<TSource> : NullableSumIterator<TSource, double>
+		{
+			public NullableDoubleSumIterator(IIteratorLink<TSource, double?> source) : base(source)
+			{
+				
+			}
+			
+			protected override double Add(double a, double b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class NullableDecimalSumIterator<TSource> : NullableSumIterator<TSource, decimal>
+		{
+			public NullableDecimalSumIterator(IIteratorLink<TSource, decimal?> source) : base(source)
+			{
+				
+			}
+			
+			protected override decimal Add(decimal a, decimal b)
+			{
+				return a + b;
+			}
+		}
+		
+		private class ProcessIterator<TSource, TIntermediate, TResult> : BasicIteratorLink<TSource, TIntermediate, TResult>
+		{
+			readonly Func<Func<TResult, bool>, Action, bool> processingFunc;
+			
+			public ProcessIterator(IIteratorLink<TSource, TIntermediate> source, Func<Func<TResult, bool>, Action, bool> processingFunc) : base(source)
+			{
+				if(processingFunc == null) throw new ArgumentNullException("processingFunc");
+				
+				this.processingFunc = processingFunc;
+			}
+			
+			public ProcessIterator(IIteratorLink<TSource, TIntermediate> source, Action<Func<TResult, bool>, Action> processingAction) : this(source, (n, c) => {processingAction(n, c); return true;})
+			{
+				if(processingAction == null) throw new ArgumentNullException("processingAction");
+			}
+			
+			protected override bool OnNextInternal(TIntermediate value)
+			{
+				return processingFunc(OnNextFinal, OnCompletedFinal);
+			}
+		}
+		
+		public static IIteratorLink<TSource, TResult> Process<TSource, TResult>(Func<Func<TResult, bool>, Action, bool> processingFunc)
+		{
+			return Process<TSource, TSource, TResult>(null, processingFunc);
+		}
+		
+		public static IIteratorLink<TSource, TResult> Process<TSource, TIntermediate, TResult>(this IIteratorLink<TSource, TIntermediate> source, Func<Func<TResult, bool>, Action, bool> processingFunc)
+		{
+			return new ProcessIterator<TSource, TIntermediate, TResult>(source, processingFunc);
+		}
+		
+		public static IIteratorLink<TSource, TResult> Process<TSource, TResult>(Action<Func<TResult, bool>, Action> processingAction)
+		{
+			return Process<TSource, TSource, TResult>(null, processingAction);
+		}
+		
+		public static IIteratorLink<TSource, TResult> Process<TSource, TIntermediate, TResult>(this IIteratorLink<TSource, TIntermediate> source, Action<Func<TResult, bool>, Action> processingAction)
+		{
+			return new ProcessIterator<TSource, TIntermediate, TResult>(source, processingAction);
 		}
 	}
 }
